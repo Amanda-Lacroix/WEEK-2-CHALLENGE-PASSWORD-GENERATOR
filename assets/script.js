a2214a2a20c69597f6ae8c33a2f291f16a4f1af8
Amanda Lacroix <amanda_lacroix@hotmail.com>
2023-02-02 17:48:43 -0400

    link js & css, test generate password function in console log

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 
 // Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -11,14 +12,21 @@ function writePassword() {
 
 }
 
+// we need character options that the computer can choose from
+// var charOptions ["ABCDEFGHIJKLMNOPQRSTUVWXYZ,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,0,1,2,3,4,5,6,7,8,9, !#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~"]
+
 // we need a function to generate the password bsed on criteria
 function generatePassword (){
-// we need character options
-var charOptions ["ABCDEFGHIJKLMNOPQRSTUVWXYZ,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,0,1,2,3,4,5,6,7,8,9, !#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~"]
+console.log(its working);
 
-// we need a prompt of the number of characters the user would like the password to be ( 8 to 128
-)
+// we need a prompt of the number of characters the user would like the password to be ( 8 to 128)
+
+
 // we need a prompt outlining the characters that will be used (upper/lower/special/number)
+// var charUsed = window.prompt("Passwords are to contain the following: Upper & Lower case, numbers and special characters");
+// if (!charUsed) {
+//   return;
+// }
 // the input needs to be validated
 // the password needs to be displayed on the page
 
@@ -41,15 +49,3 @@ return "password"
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-
-GIVEN I need a new, secure password
-WHEN I click the button to generate a password
-THEN I am presented with a series of prompts for password criteria
-WHEN prompted for password criteria
-THEN I select which criteria to include in the password
-WHEN prompted for the length of the password
-THEN I choose a length of at least 8 characters and no more than 128 characters
-WHEN asked for character types to include in the password
-THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-WHEN I answer each prompt
-THEN my input should be validated and at least one character type should be selected
